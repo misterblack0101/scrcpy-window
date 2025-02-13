cdf7d18ed33e9284f6b7a4deb3ae2c2068fe5dda
Harsh M <harshmaroo123@gmail.com>
2025-02-13 11:57:14 +0530

    Fixed compile and run issues

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('scrcpy-window.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from scrcpy-window!');
+		vscode.window.showInformationMessage('Hello World from scrcpy-window!1234');
 	});
 
 	context.subscriptions.push(disposable);
